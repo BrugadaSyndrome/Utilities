7610c532802bd0a3b2570cb4eaa0080f162397bd
i2c2-caj <i2c2.caj@gmail.com>
2015-03-21 16:30:34 -0600

    Can now update all .py files in a given directory

--- Python/version.py
+++ Python/version.py
@@ -1,4 +1,7 @@
-import os, time, sys, getopt
+import os
+import time
+import sys
+import getopt
 
 def make_version(mtime):
     # t = ['Text Day', 'Month', 'Num day', 'time', 'year']
@@ -29,25 +32,7 @@ def update_version(mtime, old_version, lines):
     print '---new: ', new_version
     '''
 
-def main(argv):
-    # Parse command line
-    try:
-      opts, args = getopt.getopt(argv,"hf:",["help=", "file="])
-    except getopt.GetoptError:
-        print "Please type 'version.py -h' for usage."
-        sys.exit(1)
-
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            print 'version.py -f <file> [-h <help>]'
-            sys.exit()
-        elif opt in ("-f", "--file"):
-            if (os.path.exists(arg)):
-                file_name = arg
-            else:
-                print "File ({0}) does not exist!".format(arg)
-                sys.exit(2)
-    
+def update_file(file_name):
     mtime = os.path.getmtime(file_name)
     lines = []
 
@@ -76,6 +61,48 @@ def main(argv):
 
     w = open(file_name, 'w')
     w.writelines(lines)
+
+def update_folder(folder_name):
+    files = os.listdir(folder_name)
+
+    for f in files:
+        if (f.split('.')[1] == 'py'):
+            update_file(folder_name + '\\' + f)
+            print 'updated: ' + f
+
+def main(argv):
+    # Parse command line
+    try:
+      opts, args = getopt.getopt(argv,"hf:d:",["help=", "file="])
+    except getopt.GetoptError:
+        print "Please type 'version.py -h' for usage."
+        sys.exit(1)
+    
+    if (not argv):
+        print "Use version.py with command line. Please type 'version.py -h' for usage."
+        sys.exit(1)
+
+    for opt, arg in opts:
+        # Help
+        if opt in ("-h", "--help"):
+            print 'version.py -f <file> -d directory [-h <help>]'
+            sys.exit()
+
+        # Update specific file  
+        elif opt in ("-f", "--file"):
+            if (os.path.exists(arg) and os.path.isfile(arg)):
+                update_file(arg)
+            else:
+                print "File ({0}) does not exist!".format(arg)
+                sys.exit(2)
+
+        # Update directory (all *.py files)
+        elif opt in ("-d", "--directory"):
+            if (os.path.exists(arg) and os.path.isdir(arg)):
+                update_folder(arg)
+            else:
+                print "Directory ({0}) does not exist!".format(arg)
+                sys.exit(3)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
