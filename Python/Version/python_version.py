6237508fd3f2258ecd40868591c23926e629928d
i2c2-caj <i2c2.caj@gmail.com>
2015-03-22 22:50:53 -0600

    working on allowing versioning for .cpp and .h files as well.

---
+++ Python/Version/python_version.py
@@ -0,0 +1,8 @@
+def update_version(mtime, old_version, lines):
+    new_version = make_version(mtime)
+    
+    if (len(old_version) != 0 and old_version[1] == 'Version:'):
+        if (old_version[2] != new_version):
+            lines.append('# Version: {0}\n'.format(new_version))
+    else:
+        lines.append('# Version: {0}\n'.format(new_version))
